a21697f98d7d2b013caa7da5b3c5ef8941b98da1
Haikal Hidayatullah <83757209+HaikalHdytullah@users.noreply.github.com>
2022-02-23 16:30:37 +0700

    new update

---
+++ script.js
@@ -0,0 +1,14 @@
+let slider = tns({
+  container: ".my-slider",
+  slideBy: 1,
+  speed: 400,
+  nav: false,
+  controlsContainer: "#controls",
+  prevButton: ".previous",
+  nextButton: ".next",
+  responsive: {
+    1600: {
+      items: 1,
+    },
+  },
+});
